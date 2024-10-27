c70ef83c60df29745e2ae6b2f637b908f3389e09
LorenaCcosta <lorenaaraujo954@gmail.com>
2024-10-27 18:45:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+class heroi{
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
 
+    atacar(){
+        let ataque;
+
+        if (this.tipo === "mago") {
+            ataque = "magia"
+            
+        } else if (this.tipo === "guerreiro") {
+            ataque = "espada"
+            
+        } else if (this.tipo === "monge") {
+            ataque = "artes marciais"
+            
+        } else if (this.tipo === "ninja") {
+            ataque = "shuriken"
+            
+        } 
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+
+}
+
+// Exemplo de uso da classe Heroi
+let heroi1= new heroi("Lorena", 19, "mago");
+heroi1.atacar();
